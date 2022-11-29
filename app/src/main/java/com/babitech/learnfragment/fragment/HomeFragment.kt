ca2c650bafe8d7b6305284cff43291ab43f16c32
Babi Mumba <babimumba243@gmail.com>
2022-11-29 22:43:39 +0200

    add enw file

--- app/src/main/java/com/babitech/learnfragment/fragment/HomeFragment.kt
+++ app/src/main/java/com/babitech/learnfragment/fragment/HomeFragment.kt
@@ -1,25 +1,37 @@
 package com.babitech.learnfragment.fragment
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.babitech.learnfragment.MainActivity
+import com.babitech.learnfragment.Movie
+import com.babitech.learnfragment.MoviesAdapter
 import com.babitech.learnfragment.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    val liste_element = ArrayList<Movie>()
+    val display = ArrayList<Movie>()
 
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        liste_element.add(Movie(R.drawable.captain_marvel,"baba","23"))
+        liste_element.add(Movie(R.drawable.captain_marvel,"bobo","29"))
+        liste_element.add(Movie(R.drawable.captain_marvel,"bibi","0"))
+        display.addAll(liste_element)
+        val monad = MoviesAdapter(display)
+        may_recycler.layoutManager = LinearLayoutManager(activity)
+        may_recycler.adapter = monad
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
