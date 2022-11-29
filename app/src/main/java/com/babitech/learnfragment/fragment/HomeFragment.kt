65a84b0e2a8646a747f4944153ca2e81c9fe9394
Babi Mumba <babimumba243@gmail.com>
2022-11-29 22:06:59 +0200

    add enw file

--- app/src/main/java/com/babitech/learnfragment/fragment/HomeFragment.kt
+++ app/src/main/java/com/babitech/learnfragment/fragment/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.babitech.learnfragment
+package com.babitech.learnfragment.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.babitech.learnfragment.R
 
 
 class HomeFragment : Fragment() {
