8ac6065a5a05d687f5f5f0e2a27722e68d4976a6
Babi Mumba <babimumba243@gmail.com>
2022-11-29 22:24:28 +0200

    add ewn file

--- app/src/main/java/com/babitech/learnfragment/MainActivity.kt
+++ app/src/main/java/com/babitech/learnfragment/MainActivity.kt
@@ -5,6 +5,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.babitech.learnfragment.fragment.HomeFragment
+import com.babitech.learnfragment.fragment.MenuFragment
+import com.babitech.learnfragment.fragment.SearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,9 +20,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadFragmant(HomeFragment())
+
         bottom_nav.setOnNavigationItemSelectedListener { menuitem ->
             when{
-                menuitem.itemId==R.id.menu
+                menuitem.itemId==R.id.menu ->{
+                    loadFragmant(MenuFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                menuitem.itemId==R.id.home ->{
+                    loadFragmant(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                menuitem.itemId==R.id.search ->{
+                    loadFragmant(SearchFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }else->{
+                    return@setOnNavigationItemSelectedListener false
+                }
             }
         }
 
