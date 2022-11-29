e9e5a47b3e80434c264436f2395c10477a5e1999
Babi Mumba <babimumba243@gmail.com>
2022-11-29 19:15:30 +0200

    add new file

--- app/src/main/java/com/babitech/learnfragment/MainActivity.kt
+++ app/src/main/java/com/babitech/learnfragment/MainActivity.kt
@@ -1,10 +1,9 @@
-package net.simplifiedcoding
+package com.babitech.learnfragment
 
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.babitech.learnfragment.R
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,38 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        refreshLayout.setOnRefreshListener {
-            fetchMovies()
-        }
-
-        fetchMovies()
 
     }
 
-    private fun fetchMovies(){
-        refreshLayout.isRefreshing = true
-
-        MoviesApi().getMovies().enqueue(object : Callback<List<Movie>> {
-            override fun onFailure(call: Call<List<Movie>>, t: Throwable) {
-                refreshLayout.isRefreshing = false
-                Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
-            }
-
-            override fun onResponse(call: Call<List<Movie>>, response: Response<List<Movie>>) {
-                refreshLayout.isRefreshing = false
-                val movies = response.body()
-
-                movies?.let {
-                    showMovies(it)
-                }
-
-            }
-
-        })
-    }
-
-    private fun showMovies(movies: List<Movie>) {
-        recyclerViewMovies.layoutManager = LinearLayoutManager(this)
-        recyclerViewMovies.adapter = MoviesAdapter(movies)
-    }
 }
